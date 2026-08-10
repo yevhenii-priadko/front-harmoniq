78c806ffe4bafaf93078dad02ae22da17885d1d6
Ign1s123 <genamisinskij@gmail.com>
2026-08-10 14:52:49 +0300

    feat: implement article creation page with image upload and form fields
    - Added NewArticlePage component for creating articles.
    - Implemented image upload functionality with preview.
    - Created form fields for article title and description.
    - Styled the component using NewArticlePage.module.css.
    - Integrated NewArticlePage into the HomePage.
    - Added Formik dependency for form management.
    - Updated sprite.svg to include camera icon.

--- app/articles/new/page.tsx
+++ app/articles/new/page.tsx
@@ -1,8 +1,76 @@
+"use client";
+import { ChangeEvent, useState } from "react";
+import css from "./NewArticlePage.module.css";
+import Image from "next/image";
+
 export default function NewArticlePage() {
+  const [preview, setPreview] = useState<string | null>(null);
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        setPreview(reader.result as string);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-    <div>
-      <h1>Create article</h1>
-      {/* TODO: приватний маршрут + форма створення статті */}
+    <div className={css.container}>
+      <h2 className={css.title}>Create an article</h2>
+      <form className={css.form} action="">
+        <div className={css.wrapper}>
+          <input
+            type="file"
+            id="imageUpload"
+            name="photo"
+            accept="image/*"
+            className={css.hiddenInput}
+            onChange={handleFileChange}
+          />
+
+          <label htmlFor="imageUpload" className={css.uploadLabel}>
+            {preview ? (
+              <Image
+                src={preview}
+                alt="Preview"
+                className={css.previewImage}
+                width={300}
+                height={200}
+              />
+            ) : (
+              <svg width="98" height="82" className={css.cameraIcon}>
+                <use href="/sprite.svg#icon-camera"></use>
+              </svg>
+            )}
+          </label>
+          <div className={css.titleWrapper}>
+            <label className={css.label} htmlFor="title">
+              Title
+            </label>
+            <input
+              className={css.inputTitle}
+              type="text"
+              name="title"
+              id="title"
+              placeholder="Enter the title"
+            />
+          </div>
+        </div>
+        <textarea
+          className={css.description}
+          name="description"
+          id="description"
+          placeholder="Enter a text"
+        ></textarea>
+        <button className={css.submit} type="submit">
+          Publish Article
+        </button>
+      </form>
     </div>
   );
 }
